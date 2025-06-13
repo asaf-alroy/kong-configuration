2121a19002f76c54b9aad4d1f969b369cb3f9e12
asi <asafalroy123@gmail.com>
2025-06-14 01:03:44 +0300

    chore: removed unnecessary comments

--- scripts/generate-kong-configmap.sh
+++ scripts/generate-kong-configmap.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Generate ConfigMap manifest from config/kong.yml
 kubectl create configmap kong-config \
   --from-file=kong.yml=config/kong.yml \
   --dry-run=client -o yaml > k8s/kong-configmap.yaml
-
-echo "ConfigMap manifest generated at k8s/kong-configmap.yaml"
