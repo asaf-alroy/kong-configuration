386dc5e4bd56d91e0b9630dd45d4d58b482c5408
asi <asafalroy123@gmail.com>
2025-06-15 16:36:53 +0300

    feat: using execSync instead of async

--- scripts/generate-kong-configmap.mjs
+++ scripts/generate-kong-configmap.mjs
@@ -1,25 +1,18 @@
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import { execSync } from 'child_process';
 import fs from 'fs';
-
-const execAsync = promisify(exec);
 
 const output = 'k8s/kong-configmap.yaml';
 const configMapName = 'kong-config';
 const kongYml = 'output/kong.yml';
 
-async function generateConfigMap() {
-    try {
-        const { stdout } = await execAsync(
-            `kubectl create configmap ${configMapName} --from-file=${kongYml} --dry-run=client -o yaml`
-        );
-        fs.writeFileSync(output, stdout);
-        console.log('Successfully generated Kong ConfigMap');
-        process.exit(0);
-    } catch (error) {
-        console.error('Failed to generate Kong ConfigMap:', error.message);
-        process.exit(1);
-    }
+try {
+    const stdout = execSync(
+        `kubectl create configmap ${configMapName} --from-file=${kongYml} --dry-run=client -o yaml`
+    );
+    fs.writeFileSync(output, stdout);
+    console.log('Successfully generated Kong ConfigMap');
+    process.exit(0);
+} catch (error) {
+    console.error('Failed to generate Kong ConfigMap:', error.message);
+    process.exit(1);
 }
-
-generateConfigMap(); 
