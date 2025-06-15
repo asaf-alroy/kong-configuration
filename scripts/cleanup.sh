36261635a1a8e387b23a1b522b7e455a7537210e
asi <asafalroy123@gmail.com>
2025-06-15 16:35:00 +0300

    feat: removed cleanup script

--- scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-kubectl delete deployment kong --ignore-not-found
-kubectl delete service kong-proxy --ignore-not-found
-kubectl delete configmap kong-config --ignore-not-found
-
-docker rmi kong:local-dev || true
