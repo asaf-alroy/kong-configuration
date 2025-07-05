e77c1a7bd02f6af55b674c880ae21636b67dedad
asi <asafalroy123@gmail.com>
2025-07-05 15:44:50 +0300

    refactor: namings

--- scripts/sync-configs.mjs
+++ scripts/sync-configs.mjs
@@ -2,18 +2,18 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 
 const KONG_ADMIN_URL = 'http://localhost:8001';
-const OUTPUT_FILE = 'config/kong.yml';
+const KONG_CONFIG_FILE = 'config/kong.yml';
 const CONFIGMAP_OUTPUT = 'output/kong-configmap.yaml';
 const CONFIGMAP_NAME = 'kong-config';
 
 const syncConfigs = () => {
     try {
-        console.log(`Syncing configuration from Kong admin to ${OUTPUT_FILE}`);
-        execSync(`deck gateway dump --kong-addr ${KONG_ADMIN_URL} --output-file ${OUTPUT_FILE} --yes`, { encoding: 'utf8' });
-        console.log(`Configuration synced to ${OUTPUT_FILE}`);
+        console.log(`Syncing configuration from Kong admin to ${KONG_CONFIG_FILE}`);
+        execSync(`deck gateway dump --kong-addr ${KONG_ADMIN_URL} --output-file ${KONG_CONFIG_FILE} --yes`, { encoding: 'utf8' });
+        console.log(`Configuration synced to ${KONG_CONFIG_FILE}`);
         
         console.log('Generating Kong ConfigMap');
-        const stdout = execSync(`kubectl create configmap ${CONFIGMAP_NAME} --from-file=${OUTPUT_FILE} --dry-run=client -o yaml`);
+        const stdout = execSync(`kubectl create configmap ${CONFIGMAP_NAME} --from-file=${KONG_CONFIG_FILE} --dry-run=client -o yaml`);
         fs.writeFileSync(CONFIGMAP_OUTPUT, stdout);
         console.log('Successfully generated Kong ConfigMap');
     } catch (error) {
